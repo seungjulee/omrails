c7d231a54630a946424e69da87c1cca0e8e985d0
SJ Lee <junkmail.sj@gmail.com>
2013-06-24 14:31:04 +0900

    Add jQuery Masonry

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -3,7 +3,7 @@ class PinsController < ApplicationController
   # GET /pins.json
   before_filter :authenticate_user!, except: [:index]
   def index
-    @pins = Pin.all
+    @pins = Pin.order("created_at desc")
 
     respond_to do |format|
       format.html # index.html.erb
